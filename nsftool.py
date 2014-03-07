e1be652821f58c1e0def506d993f2b93f47947be
jasonblanks <jason.blanks@gmail.com>
2014-03-06 19:27:42 -0500

    Update nsftool.py
    * Added Command line options.
    * Added two new functions: decrypt(-d),  crack or brute force(-c)
    
    Decrypt function clones and compacts NSF's that past the valid check.
    
    Crack or the brute force function tries every combinations of ID file Password combination found in the load file to try and brute force the nsf.   I thought this is useful given our clients, nothing else to say here.
    
    Notes:
    Steve's method seemed to not work when dealing with multiple ID files.  For some reason LN is only reading the id file listed in the INI file.  For this reason I have cleaned-up and reverted back to the first ID method.

--- nsftool.py
+++ nsftool.py
@@ -12,32 +12,120 @@
 #	IBM Notes 9 standalone client
 #	Python 2.7
 #
-import os, win32com.client
+import sys, os, win32com.client, shutil, getopt
+
 
 #change to meet your needs
 NSFPATH = "C:\\Users\\blanks\\Desktop\\TestNSF"
 IDPATH = "C:\\Users\\blanks\\Desktop\\TestNSF\\IDFiles"
-LOTUSDATAPATH = "C:\\Users\\blanks\\AppData\\Local\\IBM\\Notes\\Data"
+LotusDataPATH = "C:\\Users\\blanks\\AppData\\Local\\IBM\\Notes\\Data"
 
-#shouldn't need to change
+#shouldn't change
 LOADFILE = "load.csv"
+IDDefault = "user.id"
+IDTemp = os.path.join(LotusDataPATH,"user.id_temp")
+bad = []
 
-TASKS = [line.strip().split(',') for line in open(LOADFILE)]
+def TempFileCheck():
+	if os.path.isfile(IDTemp):
+		os.rename(IDTemp,os.path.join(LotusDataPATH,IDDefault))
+	elif os.path.isfile(os.path.join(LotusDataPATH,IDDefault)):
+		os.rename(os.path.join(LotusDataPATH,IDDefault),IDTemp)
 
-logfile = open('log.txt','w')
-logfile.write("NSF File\tID File\tPASSWORD\tMSG Count\tSTATUS\n")
-for task in TASKS:
 
-	session = win32com.client.Dispatch("Lotus.NotesSession")
-	try:
-		session.Initialize(task[2])
-		reg = session.CreateRegistration()
-		reg.SwitchToID(os.path.join(IDPATH,task[1]),task[2])
-		database = session.GetDatabase("", os.path.join(NSFPATH, task[0]))
-		docs = database.AllDocuments
-		logfile.write(str(task[0])+"\t"+str(task[1])+"\t"+str(task[2])+"\t"+str(docs.Count)+"\tGOOD\n")
-	except:
-		logfile.write(task[0]+"\t"+task[1]+"\t"+task[2]+"\tNA\tERROR: bad password or password id file mismatch\n")
-		continue
+def NSFDecrypt(db, task, NSFPATH, logfile):
+	dbclone = db.CreateFromTemplate("",os.path.join(NSFPATH, task[0])+"--decrypt", False)
+	dbclone.Compact
+	
+	OriginalDocCount = db.AllDocuments
+	CloneDocCount = dbclone.AllDocuments
+	
+	if  CloneDocCount.Count == OriginalDocCount.Count:
+		logfile.write(str(task[0])+"\t"+str(task[1])+"\t"+str(task[2])+"\t"+str(CloneDocCount.Count)+"\tGOOD\tYES\n")
+	
+	elif  CloneDocCount.Count != OriginalDocCount.Count:
+		logfile.write(str(task[0])+"\t"+str(task[1])+"\t"+str(task[2])+"\t"+str(CloneDocCount.Count)+"\tGOOD, but decypt did not match\tNO\n")
+
+def BruteForce(bad, TASKS, logfile, IDPATH, LotusDataPATH, NSFPATH):
+	for BadNSF in bad:
+		for ID in TASKS:
+			for PASSWORD in TASKS:
+				shutil.copyfile(os.path.join(IDPATH,ID[1]),os.path.join(LotusDataPATH,"user.id"))
+				session = win32com.client.Dispatch("Lotus.NotesSession")
+				
+				try:
+					session.Initialize(PASSWORD[2])
+					database = session.GetDatabase("", os.path.join(NSFPATH, BadNSF[0]))
+					docs = database.AllDocuments
+				except:
+					logfile.write(BadNSF[0]+"\t"+ID[1]+"\t"+PASSWORD[2]+"\tNA\tERROR: BF Attempt Bad\n")
+					os.remove(os.path.join(LotusDataPATH,"user.id"))
+					continue
+				logfile.write(str(BadNSF[0])+"\t"+str(ID[1])+"\t"+str(PASSWORD[2])+"\t"+str(docs.Count)+"\tGOOD\tBRUTEFORCE\n")
+				#NSFDecrypt(database, task, NSFPATH, logfile)
+				os.remove(os.path.join(LotusDataPATH,"user.id"))
+				
+def Validate(NSFPATH, IDPATH, LotusDataPATH, LOADFILE, IDDefault, IDTemp, bad, decrypt, bruteForce):
+	TASKS = [line.strip().split(',') for line in open(LOADFILE)]
+	TempFileCheck()
+	logfile = open('log.txt','w')
+	logfile.write("NSF File\tID File\tPASSWORD\tMSG Count\tSTATUS\n")
+
+	for task in TASKS:
+		shutil.copyfile(os.path.join(IDPATH,task[1]),os.path.join(LotusDataPATH,"user.id"))
+		session = win32com.client.Dispatch("Lotus.NotesSession")
 		
+		try:
+			session.Initialize(task[2])
+			database = session.GetDatabase("", os.path.join(NSFPATH, task[0]))
+			docs = database.AllDocuments
+		except:
+			logfile.write(task[0]+"\t"+task[1]+"\t"+task[2]+"\tNA\tERROR: bad password or password id file mismatch\n")
+			os.remove(os.path.join(LotusDataPATH,"user.id"))
+			bad.append(task)
+			continue
+				
+		logfile.write(str(task[0])+"\t"+str(task[1])+"\t"+str(task[2])+"\t"+str(docs.Count)+"\tGOOD\n")
+		if decrypt == 1:
+			NSFDecrypt(database, task, NSFPATH, logfile)
+		os.remove(os.path.join(LotusDataPATH,"user.id"))
+	if bruteForce == 1:
+		BruteForce(bad, TASKS, logfile, IDPATH, LotusDataPATH, NSFPATH)
 
+	TempFileCheck()
+
+def main(argv):
+	decrypt = 0
+	bruteForce = 0
+	try:
+		opts, args = getopt.getopt(argv,"hdc",)
+		print opts
+		print args
+	except getopt.GetoptError:
+		print 'test.py -h -c -d '
+		sys.exit(2)
+
+	for opt, arg in opts:
+		#print "here"
+		if opt == '-h':
+			print 'test.py -d(decrypt valid files) -c(brute force with all given id and password combinations.)'
+			sys.exit()
+		#elif opt == "":
+			#Validate(NSFPATH, IDPATH, LotusDataPATH, LOADFILE, IDDefault, IDTemp, bad, decrypt, bruteForce)
+		elif opt == "-d":
+			decrypt = True
+			Validate(NSFPATH, IDPATH, LotusDataPATH, LOADFILE, IDDefault, IDTemp, bad, decrypt, bruteForce)
+		elif opt ==  "-c":
+			bruteForce = True
+			Validate(NSFPATH, IDPATH, LotusDataPATH, LOADFILE, IDDefault, IDTemp, bad, decrypt, bruteForce)
+		else:
+			Validate(NSFPATH, IDPATH, LotusDataPATH, LOADFILE, IDDefault, IDTemp, bad, decrypt, bruteForce)
+	if not opts:
+		Validate(NSFPATH, IDPATH, LotusDataPATH, LOADFILE, IDDefault, IDTemp, bad, decrypt, bruteForce)
+		print "here"
+
+
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
